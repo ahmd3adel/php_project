f013ffbc082dcc8256fa8adf9418facaa2025f9e
ahmed adel <ahmed.adel.abdullaa@gmail.com>
2024-03-25 16:36:19 +0200

    posts insertion and retrive

--- connection_db.php
+++ connection_db.php
@@ -7,17 +7,12 @@ $password = ""; // Replace with your actual password
 $dbname = "php_pro";
 
 try {
-    // Create a new PDO instance
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     
-    // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
     
-    // If you want to set charset
-    $pdo->exec("set names utf8");
     
 } catch(PDOException $e) {
-    // Display error message if connection fails
-    echo "Connection failed: " . $e->getMessage();
 }
 ?>
